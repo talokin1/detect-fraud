55e49eef17b732febafafc8b6dfd1d50fcea9a19
Mykola Bovan <Bovan494@gmail.com>
2025-02-08 16:32:03 +0200

    Added functions remove_missing_values and handle_outliers

---
+++ Production/preprocessing_tools.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
+
+def remove_missing_values(df: pd.DataFrame, missing_theshold=0.22):
+    df_cleaned = df.dropna(axis=1, thresh=missing_theshold*len(df))
+    df_cleaned = df_cleaned.drop(columns=["user_agent", "traffic_source", "card_holder_first_name", "card_holder_last_name"], errors="ignore")
+    
+    categorical_features = df_cleaned.select_dtypes(include=["object"]).columns
+    df_cleaned[categorical_features] = df_cleaned[categorical_features].fillna("Missing")
+
+    numerical_features = df_cleaned.select_dtypes(include=["int64", "uint64", "float64"]).columns
+    df_cleaned = df_cleaned.dropna(subset=numerical_features)
+    
+    return df_cleaned
+
+def handle_outliers(df, factor=1.5):
+
+    conditions = []
+    total_outliers = 0
+    for col in df.select_dtypes(include=['number']).columns:
+        Q1 = df[col].quantile(0.25)
+        Q3 = df[col].quantile(0.75)
+        IQR = Q3 - Q1
+
+        lower_bound = Q1 - factor * IQR
+        upper_bound = Q3 + factor * IQR
+
+        num_outliers = ((df[col] < lower_bound) | (df[col] > upper_bound)).sum()
+        total_outliers += num_outliers
+
+        print(f"{col}: {num_outliers} outliers removed")
+
+        conditions.append((df[col] >= lower_bound) & (df[col] <= upper_bound))
+
+    combined_condition = conditions[0]
+    for cond in conditions[1:]:
+        combined_condition &= cond
+
+    return df.loc[combined_condition]  
+
+def normalize_numerical(df):
+    scaler = MinMaxScaler()
+    df = scaler.fit_transform(df)
+    return df
+
+def encode_categorical(df):
+    encoder = OneHotEncoder()
+    df = encoder.fit_transform(df)
+    return df
+
