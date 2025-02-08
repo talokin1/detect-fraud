b1e6f104a9525231fa4a59f3acf1cb76a9f8538e
Mykola Bovan <Bovan494@gmail.com>
2025-02-08 12:14:45 +0200

    Added pipeline without functions

---
+++ preprocessing-tools.py
@@ -0,0 +1,15 @@
+def remove_missing_values():
+    pass
+
+def handle_outliers():
+    pass
+
+def normalize_numerical(): 
+    pass
+
+def encode_categorical():
+    pass
+
+def feature_selection():
+    pass
+
