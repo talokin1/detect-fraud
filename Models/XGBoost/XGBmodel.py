f415eadb3817a9c11d6bb5f7069e527259fecf2e
Mykola Bovan <Bovan494@gmail.com>
2025-02-10 18:07:24 +0200

    And if the miracle is impossible

--- Models/XGBoost/XGBmodel.py
+++ Models/XGBoost/XGBmodel.py
@@ -3,25 +3,25 @@ import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
 import numpy as np
+import pickle
 
 
-df = pd.read_csv('C:\Edu\detect-fraud\data\prepocessed_data-mini_v2.csv')
+df = pd.read_csv(r'C:\Edu\detect-fraud\data\preprocessed_data-mini_v5.csv')
+
 X = df.drop(columns=["is_fraud"])
 y = df["is_fraud"]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-scale_pos_weight = np.sum(y_train == 0) / np.sum(y_train == 1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
 
 model = xgb.XGBClassifier(
     objective="binary:logistic",
-    scale_pos_weight=scale_pos_weight,
     eval_metric="logloss",
-    n_estimators=100,
-    learning_rate=0.1, 
-    max_depth=6, 
-    subsample=0.8, 
-    colsample_bytree=0.8, 
+    n_estimators=200,  
+    learning_rate=0.05,
+    max_depth=6,
+    subsample=0.8,
+    colsample_bytree=0.8,
     random_state=42
 )
 
@@ -34,4 +34,4 @@ print("Classification Report:\n", classification_report(y_test, y_pred))
 print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
 print("AUC-ROC Score:", roc_auc_score(y_test, y_pred_proba))
 
-
+# pickle.dump(model, open(r"C:\Edu\detect-fraud\mods\xgboost_mini.pkl", "wb"))
